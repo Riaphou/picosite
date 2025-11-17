9f87f86f6e34bcee351f1b324cc5bd9f30cf5215
Hebert Raphaël <raphounilsou@gmail.com>
2025-11-17 17:07:24 +0100

    Load sprite in the _draw function

--- home.lua
+++ home.lua
@@ -10,6 +10,7 @@ end
 function _draw()
 	cls(18)
 	print("The page content load correctly.",10,10,7)
-	
+	sprite = unpod("b64:bHo0AEkAAABaAAAA_A5weHUAQyAQEAQweGAPGIhAOE4oIA4oUA4YIA4YYAYA_g4oIA4YUCgwDphADohQDhguGA5gDhgQDhhwDhgQHisAYA4YIA4YUA==")
+	spr(sprite, 3,20)
 end
 

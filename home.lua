a8a2b565dc76d04a07001718f4f253658f56feb7
Hebert Raphaël <raphounilsou@gmail.com>
2025-11-17 19:20:38 +0100

    Change song fetching method to fetch_web_only

--- home.lua
+++ home.lua
@@ -1,7 +1,7 @@
 title = "riaphou"
 function _init()
 	sprite = unpod("b64:bHo0AEkAAABaAAAA_A5weHUAQyAQEAQweGAPGIhAOE4oIA4oUA4YIA4YYAYA_g4oIA4YUCgwDphADohQDhguGA5gDhgQDhhwDhgQHisAYA4YIA4YUA==")
-	song = fetch("https://github.com/Riaphou/picosite/raw/refs/heads/main/oldPlageMusic.sfx")
+	song = fetch_web_only("https://github.com/Riaphou/picosite/raw/refs/heads/main/oldPlageMusic.sfx")
 	
 	if song ~= nil then
 		song:poke(0x80000)

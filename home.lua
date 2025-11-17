d9dcceb98f554a3fac7fc976176710261e563d99
Hebert Raphaël <raphounilsou@gmail.com>
2025-11-17 19:11:09 +0100

    Load sprite and music in _init function

--- home.lua
+++ home.lua
@@ -1,6 +1,8 @@
 title = "riaphou"
 function _init()
+	sprite = unpod("b64:bHo0AEkAAABaAAAA_A5weHUAQyAQEAQweGAPGIhAOE4oIA4oUA4YIA4YYAYA_g4oIA4YUCgwDphADohQDhguGA5gDhgQDhhwDhgQHisAYA4YIA4YUA==")
 	song = fetch("https://github.com/Riaphou/picosite/raw/refs/heads/main/oldPlageMusic.sfx")
+	
 	if song ~= nil then
 		song:poke(0x80000)
 	end
@@ -14,7 +16,7 @@ end
 function _draw()
 	cls(18)
 	print("The page content load correctly.",10,10,7)
-	sprite = unpod("b64:bHo0AEkAAABaAAAA_A5weHUAQyAQEAQweGAPGIhAOE4oIA4oUA4YIA4YYAYA_g4oIA4YUCgwDphADohQDhguGA5gDhgQDhhwDhgQHisAYA4YIA4YUA==")
+	
 	spr(sprite, 3,20)
 end
 

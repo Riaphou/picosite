67580504bad4c85fec6bd46afe8d370cf9e0a6fb
Hebert Raphaël <raphounilsou@gmail.com>
2025-11-17 16:47:42 +0100

    Refactor init, draw, and update functions
    Refactor functions and fix indentation in home.lua

--- home.lua
+++ home.lua
@@ -1,11 +1,14 @@
 title = "Ca marche"
-init = function()
+function _init()
 	
 end
-draw = function()
+
+function _update()
+
+end
+
+function _draw()
 	print("The page content failed to load.",10,10,7)
 	rectfill(-2,-2,50,50,7)
 end
-update = function()
 
-end

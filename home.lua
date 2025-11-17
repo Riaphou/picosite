9a819f3f4f0cfec37247468e1a4cbdb868257a2b
Hebert Raphaël <raphounilsou@gmail.com>
2025-11-17 16:11:47 +0100

    Refactor functions in home.lua to remove underscores

--- home.lua
+++ home.lua
@@ -1,12 +1,12 @@
 title = "Ca marche"
-_init = function()
+init = function()
 	
 end
-_draw = function()
+draw = function()
 	cls(0)
 	print("The page content failed to load.\n")
 	print(args.err)
 end
-_update = function()
+update = function()
 
 end
